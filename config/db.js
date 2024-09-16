4d06d9a6886fbab84797ad1ee574ec9184c9c915
Javier Gonzalez <javgon2306@gmail.com>
2024-09-16 00:34:35 -0300

    removiendo node:modules

--- config/db.js
+++ config/db.js
@@ -1,7 +1,10 @@
 import Sequelize from 'sequelize';
+import dotenv from 'dotenv'
 
-const db = new Sequelize('bienesraices_node_mvc', 'root', '', {
-    host: 'localhost',
+dotenv.config({path: '.env'})
+
+const db = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASS ?? '', {
+    host: process.env.DB_HOST,
     port: 3306,
     dialect: 'mysql',
     define: {
