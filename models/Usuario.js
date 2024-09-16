f3869049ad9c1eda463761d791015f058ad58ab7
Javier Gonzalez <javgon2306@gmail.com>
2024-09-16 11:30:30 -0300

    add model

---
+++ models/Usuario.js
@@ -0,0 +1,24 @@
+import { DataTypes } from 'sequelize';
+import db from '../config/db,js';
+
+const Usuario = db.define('usuarios', {
+    nombre: {
+        //tipo de dato varchar
+        type: DataTypes.STRING,
+        // tipo de dato notnull
+        allowNull: false
+    },
+    email: {
+        type: DataTypes.STRING,
+        allowNull: false
+    },
+    password: {
+        type: DataTypes.STRING,
+        allowNull: false
+    },
+    token: DataTypes.STRING,
+    confirmado: DataTypes.BOOLEAN,
+    
+});
+
+export default Usuario
--- routes/usuarioRoutes.js
+++ routes/usuarioRoutes.js
@@ -3,8 +3,12 @@ import { formularioLogin, formularioRegistro, formularioOlvidePassword } from '.
 
 const router = express.Router();
 
+//Login
 router.get('/login', formularioLogin);
+//Registro
 router.get('/registro', formularioRegistro);
+router.post('/registro', formularioRegistro);
+//Olvide Password
 router.get('/olvide-password', formularioOlvidePassword);
 
 export default router;
