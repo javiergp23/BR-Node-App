8bacac4ce7acfc1604d26bf4f5851f5fa3f9fbcd
Javier Gonzalez <javgon2306@gmail.com>
2024-09-18 15:02:21 -0300

    implement template mail

--- helpers/emails.js
+++ helpers/emails.js
@@ -9,6 +9,20 @@ export const emailRegistro = async (datos) => {
             pass: process.env.EMAIL_PASS
         }
     })
-    console.log(datos)
+    const { email, nombre, token } = datos
+    await transport.sendMail({
+        from: '"Bienes Raices" <info@bienesraices.com.ar>',
+        to: email,
+        subject: 'Bienes Raices - Confirmación de registro',
+        html: `<h1>Bienes Raices</h1>
+        <p>Hola ${nombre},</p>
+        <p>Te hemos enviado un email de confirmación</p>
+        <p>Si no has recibido el email, revisa tu carpeta de spam</p>
+        <p>Si tienes alguna duda, no dudes en contactarnos</p>
+        <p>Bienes Raices</p>
+        
+        <a href=""</a>
 
+        <p>El enlace de confirmación es: ${token}</p>`  
+    })
 }
